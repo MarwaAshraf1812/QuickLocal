9886a28d995b022fd754096ff0d5e4e2bcbf9a54
shahendamagdy97 <shahendamagdy97@yahoo.com>
2024-07-14 02:03:12 +0300

    update serializers.py , add backend.py , signals.py , finalize views.py , finalize account app urls.py , update settings.py , update project urls.py add userprofile in models.py and add it to admins.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    activation_token = models.CharField(max_length=64, null=True, blank=True)
+    token_created_at = models.DateTimeField(null=True, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True, verbose_name=_("Phone Number"))
+    address = models.CharField(max_length=255, blank=True, null=True, verbose_name=_("Address"))
+    city = models.CharField(max_length=100, blank=True, null=True, verbose_name=_("City"))
+    state = models.CharField(max_length=100, blank=True, null=True, verbose_name=_("State"))
+    zip_code = models.CharField(max_length=20, blank=True, null=True, verbose_name=_("ZIP Code"))
+    country = models.CharField(max_length=100, blank=True, null=True, verbose_name=_("Country"))
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True, verbose_name=_("Profile Picture"))
+    date_of_birth = models.DateField(blank=True, null=True, verbose_name=_("Date of Birth"))
+    gender = models.CharField(max_length=10, blank=True, null=True, verbose_name=_("Gender"))
+    preferred_language = models.CharField(max_length=50, blank=True, null=True, verbose_name=_("Preferred Language"))
+    currency = models.CharField(max_length=10, blank=True, null=True, verbose_name=_("Currency"))
+    date_joined = models.DateTimeField(auto_now_add=True, verbose_name=_("Date Joined"))
+    last_login = models.DateTimeField(auto_now=True, verbose_name=_("Last Login"))
+    is_verified = models.BooleanField(default=False, verbose_name=_("Is Verified"))
+    order_history = models.TextField(blank=True, null=True, verbose_name=_("Order History"))
+
+    def is_token_expired(self):
+        if self.token_created_at:
+            expiration_time = self.token_created_at + timedelta(hours=48)  # Token valid for 48 hours
+            return timezone.now() > expiration_time
+        return False
+
+    def __str__(self):
+        return self.user.username
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('products.urls')),
+    path('', include('account.urls')),
+    path('token/', TokenObtainPairView.as_view()),
 ]
