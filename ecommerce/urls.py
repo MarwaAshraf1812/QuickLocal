83c41cfb421bde89a52729228cdcf09251f72292
sara-mohamd <saraamohamed0038@gmail.com>
2024-07-26 10:30:52 +0300

    updates in media and FrontEnd product's view

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
     path('', include('vendor.urls')),
     path('', include('support.urls')),
     path('', include('notifications.urls')),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
