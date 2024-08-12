41c72db076e3a19dd2d46c026a53f967dba8f92a
MarwaAshraf1812 <marashraf090@gmail.com>
2024-08-13 01:24:36 +0300

    Create a nav bar and home page without fetching api

---
+++ quicklocal-frontend/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import LandingPage from './pages/LandingPage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/Categories" element={<h1>Categories</h1>} />
+          <Route path="/shop" element={<h1>shop</h1>} />
+          <Route path="/products" element={<h1>products</h1>} />
+          <Route path="/contactUs" element={<h1>contact</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
