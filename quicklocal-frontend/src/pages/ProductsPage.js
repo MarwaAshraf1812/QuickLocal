5a0d9e8de7aeb30a4838ee871fcb2b9fa0f0e38e
MarwaAshraf1812 <marashraf090@gmail.com>
2024-08-20 01:14:11 +0300

    refactor: change the implementation of the products page

--- quicklocal-frontend/src/pages/ProductsPage.js
+++ quicklocal-frontend/src/pages/ProductsPage.js
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import apiService from '../services/apiService';
+import AsideFilters from '../components/AsideFilters';
+import './ProductsPage.css';
 
 const ProductsPage = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const location = useLocation();
+
+    const handleFilterChange = async (filters) => {
+        try {
+            setLoading(true);
+            const categoryId = new URLSearchParams(location.search).get('category');
+            const response = await apiService.getProductsByCategory(categoryId, filters);
+            setProducts(response.products);
+            setLoading(false);
+        } catch (error) {
+            setError("Failed to load products");
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
         const queryParams = new URLSearchParams(location.search);
@@ -17,8 +32,8 @@ const ProductsPage = () => {
                 try {
                     setLoading(true);
                     const response = await apiService.getProductsByCategory(categoryId);
-                    console.log('Fetched Products:', response); // Debug API response
-                    setProducts(response.products); // Extract the products array from the response
+                    console.log('Fetched Products:', response);
+                    setProducts(response.products);
                     setLoading(false);
                 } catch (error) {
                     setError("Failed to load products");
@@ -38,34 +53,49 @@ const ProductsPage = () => {
     if (error) {
         return <p>{error}</p>;
     }
+
     const baseUrl = 'http://127.0.0.1:8000';
 
     return (
         <div className="container my-5">
-        <h2 className="text-center mb-4">Products</h2>
-        <div className="row">
-            {products.map(product => (
-                <div key={product.id} className="col-md-4 col-lg-3 mb-4">
-                    <div className="card products-card shadow-sm border-light">
-                        <img
-                            src={`${baseUrl}${product.image}`}
-                            alt={product.name}
-                            className="card-img-top products-image"
-                            onError={(e) => {
-                                console.error(`Image failed to load: ${e.target.src}`);
-                                e.target.src = '/path/to/fallback-image.jpg'; // Fallback image URL
-                            }}
-                        />
-                        <div className="cards-body text-center">
-                            <h5 className="cards-title">{product.name}</h5>
-                            <p className="card-text">${product.price}</p>
-                            <a href={`/product/${product.id}`} className="btn btn-primary">View Details</a>
-                        </div>
+            <h2 className="text-center mb-5 fw-bold blue-color">Products</h2>
+            <div className="row">
+                <div className='col-md-3'>
+                    <AsideFilters onFilterChange={handleFilterChange} />
+                </div>
+                <div className="col-md-9">
+                    <div className="row">
+                        {products.map(product => (
+                            <div key={product.id} className="col-12 mb-4">
+                                <div className="card products-card d-flex flex-row shadow-sm border-light">
+                                    <div className="col-md-4 p-0">
+                                        <img
+                                            src={`${baseUrl}${product.image}`}
+                                            alt={product.name}
+                                            className="card-img-left products-image"
+                                            onError={(e) => {
+                                                console.error(`Image failed to load: ${e.target.src}`);
+                                                e.target.src = '/path/to/fallback-image.jpg'; // Fallback image URL
+                                            }}
+                                        />
+                                    </div>
+                                    <div className="col-md-8 d-flex flex-column justify-content-center p-3">
+                                        <div className="cards-body">
+                                            <h5 className="cards-title align-items-start mt-2">{product.name}</h5>
+                                            <p>{product.description}</p>
+                                            <p className="card-text">${product.price}</p>
+                                            <div className=' d-flex flex-column justify-content-center align-items-end'>
+                                                <a href={`/product/${product.id}`} className="btn btn-custom-color">View Details</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
-            ))}
+            </div>
         </div>
-    </div>
     );
 };
 
