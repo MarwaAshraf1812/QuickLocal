1b56275f9c1e3602498aeadd5f676d87b74a8aed
MarwaAshraf1812 <marashraf090@gmail.com>
2024-08-20 01:12:49 +0300

    Feat: add Filters aside component

--- quicklocal-frontend/src/services/apiService.js
+++ quicklocal-frontend/src/services/apiService.js
@@ -18,6 +18,8 @@ const apiService = {
         } catch (error) {
             throw error;
         }
+
+        
     },
 
     searchProducts: async (query) => {
@@ -46,9 +48,21 @@ const apiService = {
             throw error;
         }
     },
-    getProductFilters: async (filter) => {
+    getProductFilters: async (filters = {}) => {
+        const queryParams = new URLSearchParams();
+
+        //Add filters
+        if(filters.ordering) queryParams.append('ordering', filters.ordering);
+        if (filters.category) queryParams.append('category', filters.category);
+        if (filters.name) queryParams.append('name', filters.name);
+        if (filters.min_price) queryParams.append('min_price', filters.min_price);
+        if (filters.max_price) queryParams.append('max_price', filters.max_price);
+        if (filters.rating) queryParams.append('rating', filters.rating);
+        if (filters.tags && filters.tags.length > 0) {
+            queryParams.append('tags', filters.tags.join(','));
+        }
         try {
-            const response = await axios.get(`http://127.0.0.1:8000/products/?ordering=${filter}`);
+            const response = await axios.get(`http://127.0.0.1:8000/products/?${queryParams.toString()}`);
             return response.data;
         } catch (error) {
             throw error;
